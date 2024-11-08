461af568a53f495d01d1279bcffef5a9aceb99fa
gualter-gc <gualter@goodcalltech.com>
2024-11-08 10:02:01 +0000

    remove code for console.logging cached movies

--- hooks/usePersistentSearch.ts
+++ hooks/usePersistentSearch.ts
@@ -1,11 +1,8 @@
 import { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useQueryClient } from "@tanstack/react-query";
-import { Queries, QueryPersistanceKeys } from "../constants/query";
-import { MovieSearchQueryData } from "../types/Movie";
+import { QueryPersistanceKeys } from "../constants/query";
 
 export const usePersistentSearch = () => {
-  const queryClient = useQueryClient();
   const [query, setQuery] = useState<string>("");
   const [currentPage, setCurrentPage] = useState<number>(1);
 
@@ -36,18 +33,6 @@ export const usePersistentSearch = () => {
     }
     if (storedPage) {
       setCurrentPage(Number(storedPage));
-    }
-
-    const cachedQueryData = queryClient.getQueryData<MovieSearchQueryData>([
-      Queries.SEARCH_MOVIES,
-      storedQuery,
-    ]);
-
-    if (cachedQueryData) {
-      console.log(
-        "Cached movies found:",
-        cachedQueryData.pages.flatMap((page) => page.movies)
-      );
     }
   };
 
