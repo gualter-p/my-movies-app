b5fdcbde15307cea0a2c8ff740ba4300fa8976fb
gualter-gc <gualter@goodcalltech.com>
2024-11-08 10:09:31 +0000

    prevent breaking exhaustic-deps rule

--- hooks/useCheckBiometrics.ts
+++ hooks/useCheckBiometrics.ts
@@ -1,19 +1,20 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import * as LocalAuthentication from "expo-local-authentication";
 
 // Checks if the device we're on allows biometric auth
 export const useCheckBiometrics = () => {
-  const [isBiometricsSupported, setIsBiometricsSupported] = useState(false);
+  const [isBiometricsSupported, setIsBiometricsSupported] =
+    useState<boolean>(false);
 
-  const checkBiometrics = async () => {
+  const checkBiometrics = useCallback(async () => {
     const compatible = await LocalAuthentication.hasHardwareAsync();
     const enrolled = await LocalAuthentication.isEnrolledAsync();
     setIsBiometricsSupported(compatible && enrolled);
-  };
+  }, []);
 
   useEffect(() => {
     checkBiometrics();
-  }, []);
+  }, [checkBiometrics]);
 
   return {
     isBiometricsSupported,
